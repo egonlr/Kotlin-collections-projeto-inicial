ed2725c27c9227d5f521d763d81ac39fc871aeab
egonlr <egonlr@hotmail.com>
2022-02-07 13:39:24 -0300

    Devovendo elmentos com distinct

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,41 +1,11 @@
 package br.com.alura
 
 fun main() {
-    val banco = BancoDeNomes()
-    val nomesSalvos: Collection<String> = banco.nomes
-   // banco.nomes.add("Alex")
-    banco.salva("Alex")
-    println(nomesSalvos)
-    println(BancoDeNomes().nomes)
+    val assistiramCursoAndroid = listOf<String>("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    val assistiramAmbas = assistiramCursoAndroid + assistiramCursoKotlin
 
+    println(assistiramAmbas.distinct())
 
 }
 
-class BancoDeNomes {
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>() //Mantem os dados inseridos na lista
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = mutableListOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-
-    for (nome in nomes) {
-        println(nome)
-    }
-    println(nomes)
-    println("Tem o nome Alex? ${nomes.contains("Alex")}")
-    println("Tamanho da coleção: ${nomes.size}")
-}

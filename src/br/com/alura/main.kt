44d188f69494d9f5e2c664c7d0d982eb78d313b0
egonlr <egonlr@hotmail.com>
2022-02-08 14:19:41 -0300

    Utilizando o agrupamento

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -5,7 +5,8 @@ fun main() {
         Pedido(1, 20.0),
         Pedido(2, 60.0),
         Pedido(3, 30.0),
-        Pedido(4, 70.0)
+        Pedido(4, 70.0),
+        Pedido(5, 40.0)
     )
     println(pedidos)
     val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido ->
@@ -19,6 +20,21 @@ fun main() {
     }
     println(pedidosFreteGratis)
     println(pedidosFreteGratis[Pedido(numero=1, valor=20.0)])
+
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido -> pedido.valor > 50.00 }
+//    println(mapa)
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf("Alex", "Fran", "Gui", "Ana", "Paulo", "Egon", "Luiz")
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
+    println(agenda['A'])
 }
 
 data class Pedido(val numero: Int, val valor: Double)
